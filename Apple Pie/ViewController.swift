1928498ecdf0a5a4a923bfecd6a2f1726260a98f
Jacek Gałka <jacgalka@icloud.com>
2017-10-06 12:44:32 +0200

    Add new round functionality

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     //MARK: Properties
     var listOfWords = ["swift", "bug", "application"]
     let incorrectMovesAllowed = 7
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     var currentGame: Game!
     
     
@@ -35,6 +43,16 @@ class ViewController: UIViewController {
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
 
     //MARK: Outlets
     //Images
@@ -54,7 +72,7 @@ class ViewController: UIViewController {
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
     }
     
     
